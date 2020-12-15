855146fb364b3beb7b8e721b8531964fa4fa6160
Fancy <fancymoua@gmail.com>
2020-12-15 12:56:03 -0800

    Dismissing keyboard upon return and tap

--- MyMovieList/LandingVC.swift
+++ MyMovieList/LandingVC.swift
@@ -1,13 +1,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class LandingVC: UIViewController {
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        createDismissKeyboardTapGesture()
+        configureSearchTextField()
     }
-
+    
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        textFieldShouldReturn(searchTextField)
+    }
+    
 
 }
 
+extension LandingVC: UITextFieldDelegate {
+    func createDismissKeyboardTapGesture() {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        print(searchTextField.text)
+        searchTextField.text = ""
+        return true
+    }
+}
+
+extension LandingVC {
+    func configureSearchTextField() {
+        searchTextField.delegate = self
+        searchTextField.textColor = .darkGray
+    }
+}
