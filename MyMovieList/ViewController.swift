fca7bd203b9e772048b2200702b9ab0461d292bd
Fancy <fancymoua@gmail.com>
2020-12-15 12:26:17 -0800

    Initial Commit

---
+++ MyMovieList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMovieList
+//
+//  Created by Fancy on 12/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
