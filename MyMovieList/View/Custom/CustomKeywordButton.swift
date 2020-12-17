a3bd4d83cc813375b75aad6bb971a5f60877a368
Fancy <fancymoua@gmail.com>
2020-12-17 11:25:09 -0800

    added custom keyword buttons

---
+++ MyMovieList/View/Custom/CustomKeywordButton.swift
@@ -0,0 +1,26 @@
+
+
+import UIKit
+
+class CustomKeywordButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder : NSCoder) {
+        super.init(coder: aDecoder)
+        configure()
+    }
+    
+    func configure() {
+        backgroundColor = .systemOrange
+        layer.cornerRadius = 8
+        titleLabel?.font = UIFont(name: "Avenir Next", size: 15)
+        
+        
+        
+    }
+
+}
