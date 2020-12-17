11976ed165bd97a35efbac07ddbcba5eb102631b
Fancy <fancymoua@gmail.com>
2020-12-17 13:19:47 -0800

    Fix UI issues with trending collectionView dfdq

--- MyMovieList/Controllers/TrendingVC.swift
+++ MyMovieList/Controllers/TrendingVC.swift
@@ -7,13 +7,68 @@ class TrendingVC: UIViewController {
     @IBOutlet weak var trendingCollectionView: UICollectionView!
     
     var moviesArray = [MovieSearchResult]()
+    let baseURL = "https://api.themoviedb.org/3/trending/movie/week?api_key=65db6bef59bff99c6a4504f0ce877ade"
+    let cache = NSCache<NSString, UIImage>()
+    private let photoBaseURL = "https://image.tmdb.org/t/p/original"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         trendingCollectionView.dataSource = self
         trendingCollectionView.delegate = self
+        
+        getTrendingMovies()
 
+    }
+    
+    func getTrendingMovies() {
+        
+        let url = URL(string: baseURL)
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if let _ = error {
+                print("Trending - error making call")
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                print("Trending - 200 not returned")
+                return
+            }
+            
+            guard let data = data else {
+                print("Trending - no data retuned")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let allData = try decoder.decode(MovieDataAPI.self, from: data)
+                
+                self.moviesArray = []
+                
+                for item in allData.results {
+                    
+                    let id = item.id
+                    let title = item.title
+                    let releaseDate = item.release_date
+                    let posterPath = item.poster_path
+                    
+                    let movie = MovieSearchResult(id: id, title: title, release_date: releaseDate, poster_path: posterPath)
+                    
+                    self.moviesArray.append(movie)
+                    
+                    DispatchQueue.main.async {
+                        self.trendingCollectionView.reloadData()
+                    }
+                }
+            } catch {
+                print("Couldn't do it")
+            }
+        }
+        
+        task.resume()
+        
     }
     
 
@@ -21,13 +76,32 @@ class TrendingVC: UIViewController {
 
 extension TrendingVC: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return moviesArray.count
+        return moviesArray.count - 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeaturedCell", for: indexPath) as! FeaturedCell
         
+        var posterImage = UIImage()
+        
+        if let posterPath = self.moviesArray[indexPath.item].poster_path {
+            
+            let endpoint = self.photoBaseURL + posterPath
+            
+            let cacheKey = NSString(string: endpoint)
+            
+            if let image = cache.object(forKey: cacheKey) {
+                posterImage = image
+            } else {
+                posterImage = #imageLiteral(resourceName: "question-mark")
+            }
+        }
+        
+        let title = moviesArray[indexPath.item].title
+        
+        cell.configureCell(title: title, image: posterImage)
+         
         return cell
         
     }
