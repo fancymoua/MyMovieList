84319badb40fd6dbf1846095deac18a4ff3bce62
Fancy <fancymoua@gmail.com>
2020-12-24 09:56:23 -0800

    added year and rated label to watchlist cell

--- MyMovieList/View/Cells/WatchItemCell.swift
+++ MyMovieList/View/Cells/WatchItemCell.swift
@@ -11,6 +11,9 @@ class WatchItemCell: UITableViewCell {
     lazy var ratingStack = horizontalStackView(subviews: [starIcon, ratingLabel], spacing: 3)
     let starIcon = UIImageView(image: IconImages.ratingStar.image)
     let ratingLabel = UILabel()
+    lazy var otherInfoStackView = horizontalStackView(subviews: [yearLabel, ratedLabel], spacing: 3)
+    let yearLabel = UILabel()
+    let ratedLabel = UILabel()
     
     func configureCell(_ title: String, posterImage: UIImage, rating: String) {
         self.title.text = title
@@ -25,10 +28,15 @@ class WatchItemCell: UITableViewCell {
         addSubview(posterImage)
         addSubview(title)
         addSubview(ratingStack)
+        addSubview(otherInfoStackView)
+        
+        yearLabel.text = "2005"
+        ratedLabel.text = "PG-13"
         
         posterImage.translatesAutoresizingMaskIntoConstraints = false
         title.translatesAutoresizingMaskIntoConstraints = false
         ratingStack.translatesAutoresizingMaskIntoConstraints = false
+        otherInfoStackView.translatesAutoresizingMaskIntoConstraints = false
         
         title.textColor = .black
         title.font = UIFont(name: "Avenir Next Medium", size: 19)
@@ -44,6 +52,11 @@ class WatchItemCell: UITableViewCell {
         
         starIcon.tintColor = .systemYellow
         
+        yearLabel.font = UIFont(name: "Avenir Next Medium", size: 14)
+        ratedLabel.font = UIFont(name: "Avenir Next Medium", size: 14)
+        yearLabel.textColor = .systemGray
+        ratedLabel.textColor = .systemGray
+        
         NSLayoutConstraint.activate([
             posterImage.leadingAnchor.constraint(equalTo: leadingAnchor),
             posterImage.widthAnchor.constraint(equalToConstant: 48),
@@ -51,15 +64,20 @@ class WatchItemCell: UITableViewCell {
             posterImage.centerYAnchor.constraint(equalTo: centerYAnchor),
             
             title.leadingAnchor.constraint(equalTo: posterImage.trailingAnchor, constant: 15),
-            title.centerYAnchor.constraint(equalTo: centerYAnchor),
-            title.heightAnchor.constraint(equalToConstant: 70),
+//            title.centerYAnchor.constraint(equalTo: centerYAnchor),
+            title.topAnchor.constraint(equalTo: topAnchor, constant: 25),
+            title.heightAnchor.constraint(equalToConstant: 22),
             title.trailingAnchor.constraint(equalTo: ratingStack.leadingAnchor, constant: -10),
             
             ratingStack.centerYAnchor.constraint(equalTo: centerYAnchor),
             ratingStack.widthAnchor.constraint(equalToConstant: 50),
             ratingStack.heightAnchor.constraint(equalToConstant: 23),
-            ratingStack.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15)
+            ratingStack.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
             
+            otherInfoStackView.topAnchor.constraint(equalTo: title.bottomAnchor, constant: 5),
+            otherInfoStackView.heightAnchor.constraint(equalToConstant: 20),
+            otherInfoStackView.leadingAnchor.constraint(equalTo: posterImage.trailingAnchor, constant: 15),
+            otherInfoStackView.widthAnchor.constraint(equalToConstant: 100)
         ])
         
     }
