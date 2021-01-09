b6c17fc756cb83061b7b2fd13dd80242ef568bca
Fancy <fancymoua@gmail.com>
2021-01-08 17:48:17 -0800

    added protocol mock

--- MyMovieListTests/IDsManagerTests.swift
+++ MyMovieListTests/IDsManagerTests.swift
@@ -11,29 +11,7 @@ class IDsManagerTests: XCTestCase {
     
     func test_valid_IMDBID() {
         
-        print("Hello")
-        
-        let tmdbID = 88
-        
-        let correctIMDBID = "tt2543163"
-        
-        let mediaType = MediaType.Movie
-        
-        IDsManager.getIMDBID(id: tmdbID, type: mediaType) { (returnedID) in
-            self.waitForExpectations(timeout: 5, handler: nil)
-            print("returned id \(returnedID)")
-//            XCTAssertEqual(correctIMDBID, returnedID)
-        }
-        
-        print("Hello2")
-    }
-    
-    func test_movie_details() {
-        let tmdbID = 88
-        
-        MovieDetailsManager.getMovieDetails(tmdbID: tmdbID, mediaType: .Movie) { (daMovie) in
-            print("damovie is \(daMovie)")
-        }
+      
     }
     
 }
