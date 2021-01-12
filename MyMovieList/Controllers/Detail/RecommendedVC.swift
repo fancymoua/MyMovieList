c6a87129228921b1f93aa86a62847214acbeeeb8
Fancy <fancymoua@gmail.com>
2021-01-12 14:03:43 -0800

    cleaned up spacing of recommended titles

--- MyMovieList/Controllers/Detail/RecommendedVC.swift
+++ MyMovieList/Controllers/Detail/RecommendedVC.swift
@@ -10,48 +10,16 @@ class RecommendedVC: UIViewController {
     let specialCollectVC = SpecialCollectionsVC()
     
     private let containerView = UIView()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
         
         configure()
-        
-        addChild(specialCollectVC)
-        containerView.addSubview(specialCollectVC.view)
-        specialCollectVC.didMove(toParent: self)
-        constrainChildViewToContainerView(childView: specialCollectVC.view)
-        
-        var similarEndpoint = String()
-        
-        if thisMediaType == .Movie {
-            similarEndpoint = "https://api.themoviedb.org/3/movie/" + "\(tmdbID)" + "/recommendations?api_key=65db6bef59bff99c6a4504f0ce877ade&language=en-US&page=1"
-            specialCollectVC.mediaType = .Movie
-        } else if thisMediaType == .TV {
-            similarEndpoint = "https://api.themoviedb.org/3/tv/" + "\(tmdbID)" + "/recommendations?api_key=65db6bef59bff99c6a4504f0ce877ade&language=en-US&page=1"
-            specialCollectVC.mediaType = .TV
-        }
-        
-        specialCollectVC.getTrendingItems(trendingURL: similarEndpoint, type: thisMediaType)
-        specialCollectVC.howMany = 11
-        
-        let width = view.bounds.width
-        let padding: CGFloat = 0
-        let minimumSpacing: CGFloat = 0.0
-        
-        let availableWidth = width - (padding * 2) - (minimumSpacing * 1)
-        
-        let itemWidth = availableWidth / 4
-        
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: itemWidth + 10.5, height: itemWidth + 60)
-        layout.minimumLineSpacing = 10
-        layout.minimumInteritemSpacing = minimumSpacing
-        layout.scrollDirection = .vertical
-        specialCollectVC.collectionView.collectionViewLayout = layout
-        
-        specialCollectVC.viewMoreButton.removeFromSuperview()
+        addChildView()
+        populateCollectionView()
+        configureCollectionViewLayout()
     }
     
     private func configure() {
@@ -68,6 +36,13 @@ class RecommendedVC: UIViewController {
         ])
     }
     
+    private func addChildView() {
+        addChild(specialCollectVC)
+        containerView.addSubview(specialCollectVC.view)
+        specialCollectVC.didMove(toParent: self)
+        constrainChildViewToContainerView(childView: specialCollectVC.view)
+    }
+    
     private func constrainChildViewToContainerView(childView: UIView) {
         
         childView.translatesAutoresizingMaskIntoConstraints = false
@@ -78,5 +53,38 @@ class RecommendedVC: UIViewController {
             childView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
             childView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
         ])
+    }
+    
+    private func populateCollectionView() {
+        var similarEndpoint = String()
+        
+        if thisMediaType == .Movie {
+            similarEndpoint = "https://api.themoviedb.org/3/movie/" + "\(tmdbID)" + "/recommendations?api_key=65db6bef59bff99c6a4504f0ce877ade&language=en-US&page=1"
+            specialCollectVC.mediaType = .Movie
+        } else if thisMediaType == .TV {
+            similarEndpoint = "https://api.themoviedb.org/3/tv/" + "\(tmdbID)" + "/recommendations?api_key=65db6bef59bff99c6a4504f0ce877ade&language=en-US&page=1"
+            specialCollectVC.mediaType = .TV
+        }
+        
+        specialCollectVC.getTrendingItems(trendingURL: similarEndpoint, type: thisMediaType)
+        specialCollectVC.howMany = 14
+    }
+    
+    private func configureCollectionViewLayout() {
+        let width = view.bounds.width
+        
+        let itemWidth = width / 3.5
+        
+        print("width \(width)")
+        print("itemWidth is \(itemWidth)")
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: itemWidth, height: itemWidth + 50)
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 0.5
+        layout.scrollDirection = .vertical
+        specialCollectVC.collectionView.collectionViewLayout = layout
+        
+        specialCollectVC.viewMoreButton.removeFromSuperview()
     }
 }
