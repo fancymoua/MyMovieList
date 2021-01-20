f5332d790b59a6c1ab64dc1b0e5aade9e9f11d1a
Fancy <fancymoua@gmail.com>
2021-01-20 15:23:00 -0800

    moved special collection endpoint to constants file

--- MyMovieList/Controllers/Landing/FeaturedDirectorVC.swift
+++ MyMovieList/Controllers/Landing/FeaturedDirectorVC.swift
@@ -5,11 +5,9 @@ class FeaturedDirectorVC: SpecialCollectionsVC {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let endpoint = "https://api.themoviedb.org/3/discover/movie?api_key=65db6bef59bff99c6a4504f0ce877ade&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_crew=525&watch_region=US"
 
-        setCollectionTitle(title: "Featured Directors")
-        getResults(endpoint: endpoint, type: .Movie)
+        setCollectionTitle(title: "Featured Director: Nolan")
+        getResults(endpoint: Constants.API.featuredDirector, type: .Movie)
     }
     
 
--- MyMovieList/Utilities/Constants.swift
+++ MyMovieList/Utilities/Constants.swift
@@ -11,6 +11,7 @@ struct Constants {
         static let imageURL = "https://image.tmdb.org/t/p/"
         static let omdbAPIBaseURL = "https://www.omdbapi.com/?apikey=1383769a&i="
         static let personBaseURL = "https://api.themoviedb.org/3/person/"
+        static let featuredDirector = "https://api.themoviedb.org/3/discover/movie?api_key=65db6bef59bff99c6a4504f0ce877ade&language=en-US&region=US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_crew=525&watch_region=US"
     }
     struct Fonts {
         static let appFontRegular = "Avenir Next"
