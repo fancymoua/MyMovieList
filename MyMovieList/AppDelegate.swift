6fb0324f78521fb247f060d4d203f98d617f0c03
Fancy <fancymoua@gmail.com>
2021-01-21 09:36:44 -0800

    added Podfile and keyboard manager library

--- MyMovieList/AppDelegate.swift
+++ MyMovieList/AppDelegate.swift
@@ -1,19 +1,17 @@
-//
-//  AppDelegate.swift
-//  MyMovieList
-//
-//  Created by Fancy on 12/15/20.
-//
+
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
         return true
     }
 
---
+++ Pods/Target Support Files/Pods-MyMovieList-MyMovieListUITests/Pods-MyMovieList-MyMovieListUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyMovieList_MyMovieListUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MyMovieList_MyMovieListUITests
+@end
